e630c6ebb497dfb6ae28b4f70f4d1509f9f15a9f
kawtar el-alami <kawtarelalami737@gmail.com>
2019-08-09 08:40:01 -0400

    ft_countdown1

---
+++ ft_countdown/ft_countdown.c
@@ -0,0 +1,17 @@
+
+
+int main()
+
+{
+
+        int i;
+        i = 9;
+
+        while ( i>= 0) 
+        {
+	     write( 1, &i ,1 );
+             i--;    
+        }       
+        return 0;
+
+}
